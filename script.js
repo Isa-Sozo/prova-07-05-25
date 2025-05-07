6df1ee266e9c2b6dbcfdb4f24d3fde1920dfa92c
ISADORA SOZO DA SILVA <isadora_sozo-silva@estudante.sesisenai.org.br>
2025-05-07 08:56:22 -0300

    adicionando arquivo log e instalando coisinhas

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const fs = require('fs')
+const {v4: uuidv4} = require('uuid');
+
+function adicionalLog(nomeAluno){
+    const idUnico = uuid();
+    const dataHora = new Date().toLocaleString("pt-BR");
+    const mensagem = `${idUnico}, ${dataHora}, ${nomeAluno}/n `;
+    fs.appendFile('logs.txt', mensagem, 'utf8', (err)=>{
+        if(err){
+            console.error('Erro em escrever no arquivo', err);
+        }else{
+            console.log('Adicionado com sucesso');
+        }
+    })
+}
+adicionarLog("Isadora Sozo")
